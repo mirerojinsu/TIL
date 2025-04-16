d7c5ab344595d0e80c5131358e40265a153badca
김진수 <rlawlstn3206@gmail.com>
2025-04-16 22:44:40 +0900

    first commit

---
+++ RabbitMQ/rabbitmq-example-1/src/test/java/com/example/rabbitmqexample1/RabbitmqExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
